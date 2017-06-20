5eb2a08b273277b27ae1ca2c75e458dae10cc7a0
Emilien Muraton <emilien.muraton@arcadiagroup.co.uk>
2017-06-20 09:36:52 +0100

    Add storybook config.

---
+++ packages/fm-components/src/stories/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import SearchBar from '../components/SearchBar/SearchBar'
+
+storiesOf('', module)
+  .add('default state', () => (
+  ))
