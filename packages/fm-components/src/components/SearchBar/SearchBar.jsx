5aa4a53091745b69fd27b11d73ce40d62a1b00c4
Emilien Muraton <emilien.muraton@arcadiagroup.co.uk>
2017-06-21 11:57:50 +0100

    Update <SearchBar />.

--- packages/fm-components/src/components/SearchBar/SearchBar.jsx
+++ packages/fm-components/src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+import searchImg from './images/search-icon.svg'
 require('./SearchBar.css')
 
 export default class SearchBar extends Component {
@@ -15,7 +16,7 @@ export default class SearchBar extends Component {
         <form className="SearchBar-form" onSubmit={onSubmit}>
           <div className="SearchBar-iconParent">
             <button className="SearchBar-button" onClick={onSubmit}>
-              <img className="SearchBar-icon" alt="Search" src="/assets/images/search-icon.svg" />
+              <img className="SearchBar-icon" alt="Search" src={searchImg} />
             </button>
           </div>
           <input className="SearchBar-input" type="text" placeholder="Search for a movie, tv show, person..."/>
